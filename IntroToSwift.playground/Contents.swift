b674cfaef2dfeb7addd3616d30235573fca575ea
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2017-09-25 08:38:27 -0600

    messed with the math you can do and saw you can use 1 line useing pemdas

--- IntroToSwift.playground/Contents.swift
+++ IntroToSwift.playground/Contents.swift
@@ -2,4 +2,33 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//Explicit Type declaration with no value
+var ramen :String
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+var myAge :Int
+myAge = 123456789
+print(myAge)
+myAge = myAge - 123456781
+var someValue = 3.141592654 * 23
+someValue = someValue * 19
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Mason"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
+
+var someNumber :Int
+someNumber = 89234879423
+someNumber = someNumber + 23123213
+someNumber = someNumber - 23123213
+someNumber = someNumber * 2
+someNumber = someNumber / 4
+someNumber = someNumber - 123 + 1111111111111 / 23 * 21 - 2345678987654
+
